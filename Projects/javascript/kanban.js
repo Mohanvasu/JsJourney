4ea89fc231f92531a9be2073949fc574028a9004
Mohanvasu <mohanmgs232101@gmail.com>
2025-03-07 10:51:12 +0530

    Added save task functionality

--- Projects/javascript/kanban.js
+++ Projects/javascript/kanban.js
@@ -2,17 +2,56 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const todoBoard = document.getElementById("todo-board");
 
 const boards = document.querySelectorAll(".board");
+let modal = document.getElementById("taskModal");
+const close = document.querySelector(".close");
+const cancel = document.querySelector("#cancelTask");
+const saveTask = document.querySelector("#saveTask");
 
-addTaskBtn.addEventListener('click', () => {
-    const input = prompt("Enter Task");
-    if(!input) return;
+console.log(saveTask);
+
+saveTask.addEventListener("click", () => {
+    const taskName = document.querySelector("#taskName").value;
+    const taskPriority = document.querySelector("#taskPriority").value;
+    const taskDescription = document.querySelector("#taskDescription").value;
+    
+
     const task = document.createElement("p");
     task.classList.add("item");
     task.setAttribute("draggable",true);
-    task.innerText = input;
+    task.textContent =  taskName;
     attachDragEvents(task);
     todoBoard.appendChild(task);
+
+    hideModal();
+    clearModalContnent();
 });
+
+function clearModalContnent(){
+    document.querySelector("#taskName").value="";
+    document.querySelector("#taskDescription").value=""
+}
+
+cancel.addEventListener("click", hideModal);
+close.addEventListener("click", hideModal);
+
+function hideModal(){
+    modal.style.display="none";
+}
+
+// addTaskBtn.addEventListener('click', () => {
+//     const input = prompt("Enter Task");
+//     if(!input) return;
+//     const task = document.createElement("p");
+//     task.classList.add("item");
+//     task.setAttribute("draggable",true);
+//     task.innerText = input;
+//     attachDragEvents(task);
+//     todoBoard.appendChild(task);
+// });
+
+addTaskBtn.addEventListener('click', ()=>{
+    modal.style.display="block";
+})
 
 function attachDragEvents(target){
     target.addEventListener("dragstart", () => {
